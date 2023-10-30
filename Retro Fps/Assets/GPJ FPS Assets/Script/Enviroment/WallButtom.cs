de61f15e3793efe6e4612316b109f45e44f96103
kindlyplanet <132952789+kindlyplanet@users.noreply.github.com>
2023-10-30 10:13:56 -0300

    update 11

--- Retro Fps/Assets/GPJ FPS Assets/Script/Enviroment/WallButtom.cs
+++ Retro Fps/Assets/GPJ FPS Assets/Script/Enviroment/WallButtom.cs
@@ -5,25 +5,34 @@ using UnityEngine.SceneManagement;
 
 public class WallButtom : MonoBehaviour
 {   
-    
+
+   public float tiempoDeCambio;
+
+
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player") )
+        {      
+            Invoke("CambiarEscena",tiempoDeCambio);
+        }
+    }
+    public void CambiarEscena()
+    {
+        Scene currentScene = SceneManager.GetActiveScene();
+
+        int currentSceneIndex = currentScene.buildIndex;
+
+        int nextSceneIndex = currentSceneIndex + 1;
+
+        if (nextSceneIndex < SceneManager.sceneCountInBuildSettings)
         {
-          Scene currentScene = SceneManager.GetActiveScene();
-
-          int currentSceneIndex = currentScene.buildIndex;
-
-          int nextSceneIndex = currentSceneIndex + 1;
-
-          if (nextSceneIndex < SceneManager.sceneCountInBuildSettings)
-          {
             SceneManager.LoadScene(nextSceneIndex);
-          }
-          else
-          {
+       
+        }
+        else
+        {
             Debug.LogWarning("No hay una siguiente escena disponible");
-          }
         }
     }
 
